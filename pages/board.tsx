2a581b8de63e37d0e664db774ff1f53618c2028c
nikosetiawanp <nikosetiawanp@gmail.com>
2023-07-11 19:44:29 +0700

    added navbar

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,3 +1,32 @@
+import LogoMobile from "../assets/logo-mobile.svg";
+import IconChevronDown from "../assets/icon-chevron-down.svg";
+import IconPlus from "../assets/icon-plus.svg";
+import Image from "next/image";
+import IconVerticalEllipsis from "../assets/icon-vertical-ellipsis.svg";
+
 export default function Board() {
-  return <h1 className="text-heading-xl">This is board page!</h1>;
+  return (
+    // BACKGROUND PAGE
+    <div className="bg-very-dark-grey h-screen w-screen">
+      <nav className="bg-dark-grey h-[65px] flex items-center gap-4 p-4">
+        <Image priority src={LogoMobile} alt="logo-mobile" />
+        <button className="text-heading-lg text-white flex items-center gap-4">
+          Platform Launch
+          <Image priority src={IconChevronDown} alt="icon-chevron-down" />
+        </button>
+        <div className="flex justify-center items-center gap-4 ml-auto">
+          <button className="bg-main-purple w-[48px] h-[32px] rounded-full text-heading-md flex justify-center items-center hover:cursor-pointer hover:bg-main-purple-hover">
+            <Image priority src={IconPlus} alt="icon-plus" />
+          </button>
+          <button className="hover:cursor-pointer">
+            <Image
+              priority
+              src={IconVerticalEllipsis}
+              alt="icon-vertical-ellipsis"
+            />
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
 }
